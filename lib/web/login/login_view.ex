1eff949c8d4536dc18842157a5327cdb2a348dea
ivan minutillo <bernini@inventati.org>
2020-11-17 17:37:39 +0100

    wip login

--- lib/web/login/login_view.ex
+++ lib/web/login/login_view.ex
@@ -1,3 +1,7 @@
 defmodule Bonfire.Me.Web.LoginView do
   use Bonfire.Web, [:view, Application.get_env(:bonfire_me, :templates_path)]
+
+  alias Bonfire.Web.Layout.HeaderGuestLive
+  alias Bonfire.Web.Layout.LogoHeaderLive
+  alias Bonfire.Web.Layout.LoginLive
 end
