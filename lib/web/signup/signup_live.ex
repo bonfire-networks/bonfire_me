2dbe805378514aee4443832c80c25cd7c13dfdc1
bernini <bernini@inventati.org>
2020-11-22 21:11:44 +0100

    signin and signup

--- lib/web/signup/signup_live.ex
+++ lib/web/signup/signup_live.ex
@@ -4,6 +4,7 @@ defmodule Bonfire.Me.Web.SignupLive do
   alias Bonfire.Me.Web.HeroProfileLive
   alias Bonfire.Me.Web.ProfileNavigationLive
   alias Bonfire.Me.Web.ProfileAboutLive
+  alias Bonfire.Web.Components.SignupLive
   alias Bonfire.Me.Fake
   alias Bonfire.Common.Web.LivePlugs
   alias Ecto.Changeset
