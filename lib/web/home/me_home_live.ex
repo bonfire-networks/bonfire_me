4a047f94ba9d6ab6a68be3df574b781642aa5f5f
jjl <james.laver@gmail.com>
2020-11-23 13:00:11 +0000

    me home

---
+++ lib/web/home/me_home_live.ex
@@ -0,0 +1,41 @@
+defmodule Bonfire.Me.Web.MeHomeLive do
+  use Bonfire.Web, :live_view
+  alias Bonfire.Fake
+  alias Bonfire.Common.Web.LivePlugs
+  alias Bonfire.Me.Users
+  alias Bonfire.Me.Web.{CreateUserLive, MeHomeLive}
+
+  def mount(params, session, socket) do
+    LivePlugs.live_plug params, session, socket, [
+      LivePlugs.LoadCurrentAccountFromSession,
+      LivePlugs.LoadCurrentUserFromPath,
+      LivePlugs.StaticChanged,
+      LivePlugs.Csrf,
+      &mounted/3,
+    ]
+  end
+
+  defp mounted(params, session, socket) do
+    {:ok, socket
+    |> assign(page_title: "Switch User",
+    selected_tab: "about",
+    current_account: socket.assigns.current_account,
+    users: Users.by_account(socket.assigns.current_account))}
+
+  end
+
+  # def handle_params(%{"tab" => tab} = _params, _url, socket) do
+  #   {:noreply,
+  #    assign(socket,
+  #      selected_tab: tab
+  #    )}
+  # end
+
+  # def handle_params(%{} = _params, _url, socket) do
+  #   {:noreply,
+  #    assign(socket,
+  #      current_user: Fake.user_live()
+  #    )}
+  # end
+
+end
