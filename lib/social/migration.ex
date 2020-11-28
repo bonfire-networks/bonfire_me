f9f0235e4190e50d80f4d2bad70f7eca603abead
jjl <james.laver@gmail.com>
2020-11-28 14:59:19 +0000

    update social migration

--- lib/social/migration.ex
+++ lib/social/migration.ex
@@ -13,10 +13,10 @@ defmodule Bonfire.Me.Social.Migration do
       require Bonfire.Data.Social.FollowCount.Migration
       require Bonfire.Data.Social.Like.Migration
       require Bonfire.Data.Social.LikeCount.Migration
+      require Bonfire.Data.Social.Mention.Migration
       require Bonfire.Data.Social.Profile.Migration
       Bonfire.Data.Social.Block.Migration.migrate_block()
       Bonfire.Data.Social.Bookmark.Migration.migrate_bookmark()
-      Bonfire.Data.Social.Profile.Migration.migrate_profile()
       Bonfire.Data.Social.Character.Migration.migrate_character()
       Bonfire.Data.Social.Circle.Migration.migrate_circle()
       Bonfire.Data.Social.Encircle.Migration.migrate_encircle()
@@ -24,6 +24,8 @@ defmodule Bonfire.Me.Social.Migration do
       Bonfire.Data.Social.FollowCount.Migration.migrate_follow_count()
       Bonfire.Data.Social.Like.Migration.migrate_like()
       Bonfire.Data.Social.LikeCount.Migration.migrate_like_count()
+      Bonfire.Data.Social.Mention.Migration.migrate_mention()
+      Bonfire.Data.Social.Profile.Migration.migrate_profile()
     end
   end
 
@@ -38,8 +40,10 @@ defmodule Bonfire.Me.Social.Migration do
       require Bonfire.Data.Social.FollowCount.Migration
       require Bonfire.Data.Social.Like.Migration
       require Bonfire.Data.Social.LikeCount.Migration
+      require Bonfire.Data.Social.Mention.Migration
       require Bonfire.Data.Social.Profile.Migration
       Bonfire.Data.Social.Profile.Migration.migrate_profile()
+      Bonfire.Data.Social.Mention.Migration.migrate_mention()
       Bonfire.Data.Social.LikeCount.Migration.migrate_like_count()
       Bonfire.Data.Social.Like.Migration.migrate_like()
       Bonfire.Data.Social.FollowCount.Migration.migrate_follow_count()
