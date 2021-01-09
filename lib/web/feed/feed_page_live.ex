7db34f0050af1b515cca608d8bc2107c507ae283
bernini <bernini@inventati.org>
2021-01-09 16:43:32 +0100

    auth ui flow

---
+++ lib/web/feed/feed_page_live.ex
@@ -0,0 +1,41 @@
+defmodule Bonfire.UI.Social.FeedPageLive do
+  use Bonfire.Web, :live_view
+  alias Bonfire.Fake
+  alias Bonfire.Common.Web.LivePlugs
+  alias Bonfire.Me.Identity.Users
+  alias Bonfire.Me.Web.{CreateUserLive}
+  alias Bonfire.UI.Social.FeedLive
+
+  def mount(params, session, socket) do
+    LivePlugs.live_plug params, session, socket, [
+      LivePlugs.LoadCurrentAccount,
+      LivePlugs.LoadCurrentUser,
+      LivePlugs.LoadCurrentAccountUsers,
+      LivePlugs.StaticChanged,
+      LivePlugs.Csrf,
+      &mounted/3,
+    ]
+  end
+
+  defp mounted(params, session, socket) do
+    title = "@#{socket.assigns.current_user.character.username}'s feed"
+    {:ok, socket
+    |> assign(page_title: "My Feed", feed_title: title)}
+  end
+
+
+  # def handle_params(%{"tab" => tab} = _params, _url, socket) do
+  #   {:noreply,
+  #    assign(socket,
+  #      selected_tab: tab
+  #    )}
+  # end
+
+  # def handle_params(%{} = _params, _url, socket) do
+  #   {:noreply,
+  #    assign(socket,
+  #      current_user: Fake.user_live()
+  #    )}
+  # end
+
+end
