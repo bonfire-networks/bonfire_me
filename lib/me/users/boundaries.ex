70aae2f659a8ad392b1c395b08f141bd6d4eb62d
Mayel <git@mayel.space>
2021-03-20 23:18:21 +0100

    WIP

--- lib/me/users/boundaries.ex
+++ lib/me/users/boundaries.ex
@@ -1,10 +1,11 @@
 defmodule Bonfire.Me.Users.Boundaries do
   alias Bonfire.Data.Identity.User
 
-  def maybe_grant_read_to_circles(%User{}=user, %{id: object_id} = _object, circle_ids) when is_list(circle_ids) do
+  @doc "Grant access to an object to a list of circles + the user"
+  def maybe_grant_read_to_circles(%User{id: user_id}=user, %{id: object_id} = _object, circle_ids) when is_list(circle_ids) do
     with {:ok, %{id: acl_id}} <- Bonfire.Me.Users.Acls.create(user, nil),
     {:ok, controlled} <- Bonfire.Boundaries.Controlleds.create(%{id: object_id, acl_id: acl_id}),
-    {:ok, grants} <- Bonfire.Boundaries.Grants.grant(circle_ids, acl_id) do
+    {:ok, grants} <- Bonfire.Boundaries.Grants.grant(circle_ids ++ [user_id], acl_id) do
       :ok
     end
   end
