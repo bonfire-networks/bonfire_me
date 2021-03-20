854b9ae02505451f1097d787c444cdf1797ddc81
Mayel <git@mayel.space>
2021-03-20 23:30:35 +0100

    add least add creator

--- lib/me/users/boundaries.ex
+++ lib/me/users/boundaries.ex
@@ -2,6 +2,8 @@ defmodule Bonfire.Me.Users.Boundaries do
   alias Bonfire.Data.Identity.User
 
   @doc "Grant access to an object to a list of circles + the user"
+  def maybe_grant_read_to_circles(user, object, circle_ids \\ [])
+
   def maybe_grant_read_to_circles(%User{id: user_id}=user, %{id: object_id} = _object, circle_ids) when is_list(circle_ids) do
     with {:ok, %{id: acl_id}} <- Bonfire.Me.Users.Acls.create(user, nil),
     {:ok, controlled} <- Bonfire.Boundaries.Controlleds.create(%{id: object_id, acl_id: acl_id}),
@@ -9,6 +11,11 @@ defmodule Bonfire.Me.Users.Boundaries do
       :ok
     end
   end
+
+  def maybe_grant_read_to_circles(user, object, _) do
+    maybe_grant_read_to_circles(user, object, []) # add creator to grants
+  end
+
   def maybe_grant_read_to_circles(_, _, _), do: :skipped
 
 end
