e41fe1a84484148ce48f0d99b018836cc2405d97
Mayel <git@mayel.space>
2021-06-19 17:22:27 +0200

    boundaries debug WIP

--- lib/me/boundaries/boundaries.ex
+++ lib/me/boundaries/boundaries.ex
@@ -1,31 +1,31 @@
 defmodule Bonfire.Me.Users.Boundaries do
   alias Bonfire.Data.Identity.User
 
-  def maybe_make_visible_for(user, object, circle_ids \\ []), do: maybe_grant_access_to(user, object, circle_ids, :read_only)
+  def maybe_make_visible_for(current_user, object, circle_ids \\ []), do: maybe_grant_access_to(current_user, object, circle_ids, :read_only)
 
   @doc "Grant access to an object to a list of circles + the user"
-  def maybe_grant_access_to(user, object, circle_ids \\ [], access \\ :read_only)
+  def maybe_grant_access_to(current_user, object, circle_ids \\ [], access \\ :read_only)
 
-  def maybe_grant_access_to(%User{id: user_id}=user, %{id: object_id} = _object, circle_ids, access) when is_list(circle_ids) do
+  def maybe_grant_access_to(%User{id: user_id}=current_user, %{id: object_id} = _object, circle_ids, access) when is_list(circle_ids) do
 
-    grant_subjects = (circle_ids ++ [user_id]) #|> IO.inspect(label: "grant_subjects")
+    grant_subjects = (circle_ids ++ [user_id]) #|> IO.inspect(label: "maybe_grant_access_to")
 
-    with {:ok, %{id: acl_id}} <- Bonfire.Me.Users.Acls.create(user, nil),
-    {:ok, _controlled} <- Bonfire.Boundaries.Controlleds.create(%{id: object_id, acl_id: acl_id}),
-    {:ok, grants} <- Bonfire.Boundaries.Grants.grant(grant_subjects, acl_id, access) do
-      # IO.inspect(grants: grants)
+    with {:ok, %{id: acl_id}} <- Bonfire.Me.Users.Acls.create(current_user, nil),# |> IO.inspect(label: "acled"),
+    {:ok, _controlled} <- Bonfire.Boundaries.Controlleds.create(%{id: object_id, acl_id: acl_id}), #|> IO.inspect(label: "ctled"),
+    {:ok, grant} <- Bonfire.Boundaries.Grants.grant(grant_subjects, acl_id, access) do # |> IO.inspect(label: "granted") do
+      # IO.inspect(one_grant: grant)
       {:ok, :granted}
     else
-      grants when is_list(grants) ->
-        # IO.inspect(grants: grants)
+      grants when is_list(grants) -> # FIXME
+        # IO.inspect(many_grants: grants)
         {:ok, :granted}
 
       e -> {:error, e}
     end
   end
 
-  def maybe_grant_access_to(user, object, circle, access) do
-    maybe_grant_access_to(user, object, [circle], access)
+  def maybe_grant_access_to(current_user, object, circle, access) do
+    maybe_grant_access_to(current_user, object, [circle], access)
   end
 
   def maybe_grant_access_to(_, _, _, _), do: :skipped
