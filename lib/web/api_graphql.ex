946dabc37a17c19d403ccf208251059cbf521cad
Mayel de Borniol <git@mayel.space>
2021-12-03 18:35:19 +1300

    API

--- lib/web/api_graphql.ex
+++ lib/web/api_graphql.ex
@@ -12,19 +12,19 @@ defmodule Bonfire.Me.API.GraphQL do
     field :posts, list_of(:post) do
       arg :paginate, :paginate # TODO
 
-      resolve dataloader(Bonfire.Data.Identity.User)
+      resolve dataloader(Pointers.Pointer)
     end
 
     field :user_activities, list_of(:activity) do
       arg :paginate, :paginate # TODO
 
-      resolve dataloader(Bonfire.Data.Identity.User)
+      resolve dataloader(Pointers.Pointer)
     end
 
     field :boost_activities, list_of(:activity) do
       arg :paginate, :paginate # TODO
 
-      resolve dataloader(Bonfire.Data.Identity.User)
+      resolve dataloader(Pointers.Pointer)
     end
 
   end
@@ -41,22 +41,34 @@ defmodule Bonfire.Me.API.GraphQL do
       resolve &my_feed/3
     end
 
+    field :user_notifications, list_of(:activity) do
+      arg :paginate, :paginate # TODO
+
+      resolve &my_notifications/3
+    end
+
+    field :flags_for_moderation, list_of(:activity) do
+      arg :paginate, :paginate # TODO
+
+      resolve &all_flags/3
+    end
+
     field :like_activities, list_of(:activity) do
       arg :paginate, :paginate # TODO
 
-      resolve dataloader(Bonfire.Data.Identity.User)
+      resolve dataloader(Pointers.Pointer)
     end
 
     field :followers, list_of(:follow) do
       arg :paginate, :paginate # TODO
 
-      resolve dataloader(Bonfire.Data.Identity.User, args: %{my: :followers})
+      resolve dataloader(Pointers.Pointer, args: %{my: :followers})
     end
 
     field :followed, list_of(:follow) do
       arg :paginate, :paginate # TODO
 
-      resolve dataloader(Bonfire.Data.Identity.User, args: %{my: :followed})
+      resolve dataloader(Pointers.Pointer, args: %{my: :followed})
     end
 
   end
@@ -97,34 +109,46 @@ defmodule Bonfire.Me.API.GraphQL do
 
   end
 
-  def get_user(_parent, %{filter: %{username: username}}, info) do
+  defp get_user(_parent, %{filter: %{username: username}}, info) do
     Bonfire.Me.Users.by_username(username)
   end
 
-  def get_user(_parent, %{filter: %{id: id}}, info) do
+  defp get_user(_parent, %{filter: %{id: id}}, info) do
     Bonfire.Me.Users.by_id(id)
   end
 
-  def get_user(%Bonfire.Data.Identity.User{} = parent, args, info) do
+  defp get_user(%Bonfire.Data.Identity.User{} = parent, args, info) do
     # IO.inspect(parent: parent)
     {:ok, parent}
   end
 
-  def get_user(_parent, args, info) do
+  defp get_user(_parent, args, info) do
     # IO.inspect(args: args)
     {:ok, GraphQL.current_user(info)}
   end
 
-  def get_user(_args, info) do
+  defp get_user(_args, info) do
     {:ok, GraphQL.current_user(info)}
   end
 
-  def my_feed(%Bonfire.Data.Identity.User{} = parent, args, info) do
-    # IO.inspect(parent: parent)
+  defp my_feed(%Bonfire.Data.Identity.User{} = parent, args, info) do
+    Bonfire.Social.FeedActivities.my_feed(parent)
+    |> feed()
+  end
+
+  defp my_notifications(%Bonfire.Data.Identity.User{} = parent, args, info) do
+    Bonfire.Social.FeedActivities.feed(:notifications, parent)
+    |> feed()
+  end
+
+  defp all_flags(%Bonfire.Data.Identity.User{} = parent, args, info) do
+    Bonfire.Social.Flags.list(parent)
+    |> feed()
+  end
+
+  defp feed(%{entries: feed}) when is_list(feed) do
     {:ok,
-      Bonfire.Social.FeedActivities.my_feed(parent)
-      |> Map.get(:entries) # TODO: handle pagination
-      # |> IO.inspect()
+      feed
       |> Enum.map(& Map.get(&1, :activity))
     }
   end
