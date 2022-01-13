d1ca948576a3864be66faccb24e65ecadbd6d33d
Mayel de Borniol <git@mayel.space>
2022-01-13 20:23:08 +1300

    signup fix

--- lib/web/views/signup/signup_live.ex
+++ lib/web/views/signup/signup_live.ex
@@ -1,18 +1,20 @@
 defmodule Bonfire.Me.Web.SignupLive do
-  use Bonfire.Web, {:surface_view, [layout: {Bonfire.UI.Social.Web.LayoutView, "empty_template.html"}]}
+  use Bonfire.Web, {:surface_view, [layout: {Bonfire.UI.Social.Web.LayoutView, "without_sidebar.html"}]}
   alias Bonfire.Me.Accounts
 
   # because this isn't a live link and it will always be accessed by a
   # guest, it will always be offline
   def mount(params, session, socket) do
-    # IO.inspect(session: session)
+    IO.inspect(session: session)
     {:ok,
      socket
+      |> assign(:invite, e(session, "invite", nil))
       |> assign_new(:current_account, fn -> nil end)
       |> assign_new(:current_user, fn -> nil end)
       |> assign_new(:registered, fn -> false end)
       |> assign_new(:error, fn -> nil end)
-      |> assign_new(:form, fn -> form_cs(session) end)}
+      |> assign_new(:form, fn -> form_cs(session) end)
+    }
   end
 
   defp form_cs(%{"invite" => invite}), do: Accounts.changeset(:signup, %{}, invite: invite)
