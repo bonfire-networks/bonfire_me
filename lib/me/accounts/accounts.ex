bf3b5b585f944f9752df6e3c78e289b27a423fbb
Karen Konou <konoukaren@gmail.com>
2022-01-19 22:56:05 +0100

    Fix fake

--- lib/me/accounts/accounts.ex
+++ lib/me/accounts/accounts.ex
@@ -82,7 +82,7 @@ defmodule Bonfire.Me.Accounts do
 
   def signup(%Changeset{data: %Account{}}=cs, opts) do
     special_invite = System.get_env("INVITE_KEY_EMAIL_CONFIRMATION_BYPASS")
-    opts = Keyword.put_new(opts, :must_confirm?, not (opts[:invite] && special_invite && opts[:invite] == special_invite))
+    opts = Keyword.put_new(opts, :must_confirm?, not (Keyword.get(opts, :invite, false) && special_invite && Keyword.get(opts, :invite, false) == special_invite))
 
     if cs.valid? do
       repo().transact_with fn -> # revert if email send fails
