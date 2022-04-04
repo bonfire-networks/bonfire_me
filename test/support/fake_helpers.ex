eb24e334cee133609d6a627d43ddf37d556f7ab1
Mayel de Borniol <git@mayel.space>
2022-04-04 15:22:50 +1200

    clean

--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -1,14 +1,4 @@
 defmodule Bonfire.Me.Test.FakeHelpers do
 
-  alias Bonfire.Data.Identity.Account
-  alias Bonfire.Me.Fake
-  alias Bonfire.Me.{Accounts, Users}
-  import ExUnit.Assertions
-
-  import Bonfire.Me.Integration
-
-  import Bonfire.Me.Fake
-
-
 
 end
