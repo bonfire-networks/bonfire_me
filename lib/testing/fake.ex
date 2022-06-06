2baf4355517fa2220bdc98e7c13449b0eb00cfd1
Mayel de Borniol <git@mayel.space>
2022-06-06 13:21:59 +1200

    Avoid reloading settings in tests

--- lib/testing/fake.ex
+++ lib/testing/fake.ex
@@ -11,7 +11,9 @@ defmodule Bonfire.Me.Fake do
     {:ok, account} = signup_form(attrs)
     # |> dump
     |> Accounts.signup(..., opts)
+
     account
+    |> Map.put(:settings, nil)
   end
 
   # def fake_account!(attrs \\ %{}) do
@@ -27,21 +29,21 @@ defmodule Bonfire.Me.Fake do
 
     with {:ok, user} <- Users.create(create_user_form(attrs), account) do
       user
+      |> Map.put(:settings, nil)
     else
       {:error, %Ecto.Changeset{}} when is_binary(custom_username) ->
         Users.by_username!(custom_username)
     end
   end
-  # def fake_user!(%Account{}=account \\ %{}, attrs \\ %{}) do
-  #   assert {:ok, user} = Users.create(Fake.user(attrs), account)
-  #   user
-  # end
+
   def fake_user!(name, user_attrs) when is_binary(name) do
-    fake_account!() |> fake_user!(Map.merge(user_attrs, %{profile: %{name: name}, character: %{username: name}}))
+    fake_account!()
+    |> fake_user!(Map.merge(user_attrs, %{profile: %{name: name}, character: %{username: name}}))
   end
 
   def fake_user!(account_attrs, user_attrs) do
-    fake_account!(account_attrs) |> fake_user!(user_attrs)
+    fake_account!(account_attrs)
+    |> fake_user!(user_attrs)
   end
 
 end
