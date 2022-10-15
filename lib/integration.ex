fc1cee55883e3690bf2634e22146e801125352bb
Mayel de Borniol <git@mayel.space>
2022-10-16 11:01:15 +1300

    fix

--- lib/integration.ex
+++ lib/integration.ex
@@ -1,6 +1,6 @@
 defmodule Bonfire.Me.Integration do
   alias Bonfire.Common.Config
-  # alias Bonfire.Common.Utils
+  alias Bonfire.Common.Utils
   import Untangle
 
   def repo, do: Config.get!(:repo_module)
