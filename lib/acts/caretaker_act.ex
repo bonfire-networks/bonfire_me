59a5ade8d93c247980825875495bef65ea0e8075
Mayel de Borniol <git@mayel.space>
2022-12-09 00:53:50 +1300

    acts

---
+++ lib/acts/caretaker_act.ex
@@ -0,0 +1,115 @@
+defmodule Bonfire.Me.Acts.Caretaker do
+  @moduledoc """
+  An act that deals with maintaining a `Caretaker` record for a changeset.
+
+  During insertion, adds an associated insert if a caretaker can be found in the epic options.
+
+  During deletion, ensures that the related record will be cleaned up.
+
+  Epic Options (insert):
+    * `:caretaker` - user that will take care of the post, falls back to `:current_user`
+    * `:current_user` - user that will taker care of the post, fallback if `:caretaker` is not set.
+
+  Act Options:
+    * `:on` - key to find changeset, required.
+  """
+
+  alias Bonfire.Epics
+  alias Bonfire.Epics.Act
+  alias Bonfire.Epics.Epic
+
+  alias Ecto.Changeset
+  import Epics
+  use Arrows
+
+  def run(epic, act) do
+    on = act.options[:on]
+    changeset = epic.assigns[on]
+    current_user = epic.assigns[:options][:current_user]
+
+    cond do
+      epic.errors != [] ->
+        maybe_debug(
+          epic,
+          act,
+          length(epic.errors),
+          "Skipping due to epic errors"
+        )
+
+        epic
+
+      is_nil(on) or not is_atom(on) ->
+        maybe_debug(epic, act, on, "Skipping due to `on` option")
+        epic
+
+      not is_struct(changeset) || changeset.__struct__ != Changeset ->
+        maybe_debug(epic, act, changeset, "Skipping :#{on} due to changeset")
+        epic
+
+      changeset.action not in [:insert, :upsert, :delete] ->
+        maybe_debug(
+          epic,
+          act,
+          changeset.action,
+          "Skipping, no matching action on changeset"
+        )
+
+        epic
+
+      changeset.action in [:insert, :upsert] ->
+        case epic.assigns[:options][:caretaker] do
+          %{id: id} ->
+            maybe_debug(epic, act, id, "Casting explicit caretaker")
+            cast(epic, act, changeset, on, id)
+
+          id when is_binary(id) ->
+            maybe_debug(epic, act, id, "Casting explicit caretaker")
+            cast(epic, act, changeset, on, id)
+
+          nil ->
+            case current_user do
+              %{id: id} ->
+                Epics.smart(
+                  epic,
+                  act,
+                  current_user,
+                  "Casting current user as caretaker #{id}"
+                )
+
+                cast(epic, act, changeset, on, id)
+
+              id when is_binary(id) ->
+                maybe_debug(epic, act, id, "Casting current user as caretaker")
+                cast(epic, act, changeset, on, id)
+
+              other ->
+                Epics.smart(
+                  epic,
+                  act,
+                  current_user,
+                  "Skipping because of current user"
+                )
+
+                epic
+            end
+
+          other ->
+            Epics.smart(epic, act, other, "Invalid custom caretaker")
+            epic
+        end
+
+      changeset.action == :delete ->
+        # TODO: deletion
+        epic
+    end
+  end
+
+  defp cast(epic, act, changeset, on, caretaker_id) do
+    id = Changeset.get_field(changeset, :id)
+
+    changeset
+    |> Changeset.cast(%{caretaker: %{id: id, caretaker_id: caretaker_id}}, [])
+    |> Changeset.cast_assoc(:caretaker)
+    |> Epic.assign(epic, on, ...)
+  end
+end
---
+++ lib/acts/creator_act.ex
@@ -0,0 +1,117 @@
+defmodule Bonfire.Me.Acts.Creator do
+  @moduledoc """
+  An act that deals with maintaining a `Created` record for a changeset.
+
+  During insertion, adds an associated insert if a creator can be found in the epic options.
+
+  During deletion, ensures that the related record will be cleaned up.
+
+  Epic Options (insert):
+    * `:creator` - user that will create the post, falls back to `:current_user`
+    * `:current_user` - user that will create the post, fallback if `:creator` is not set.
+
+  Act Options:
+    * `:on` - key to find changeset, required.
+  """
+
+  alias Bonfire.Epics
+  alias Bonfire.Epics.Act
+  alias Bonfire.Epics.Epic
+
+  alias Ecto.Changeset
+  import Epics
+  use Arrows
+
+  # see module documentation
+  @doc false
+  def run(epic, act) do
+    on = act.options[:on]
+    changeset = epic.assigns[on]
+    current_user = epic.assigns[:options][:current_user]
+
+    cond do
+      epic.errors != [] ->
+        maybe_debug(
+          epic,
+          act,
+          length(epic.errors),
+          "Skipping due to epic errors"
+        )
+
+        epic
+
+      is_nil(on) or not is_atom(on) ->
+        maybe_debug(epic, act, on, "Skipping due to `on` option")
+        epic
+
+      not is_struct(changeset) || changeset.__struct__ != Changeset ->
+        maybe_debug(epic, act, changeset, "Skipping :#{on} due to changeset")
+        epic
+
+      changeset.action not in [:insert, :delete] ->
+        maybe_debug(
+          epic,
+          act,
+          changeset.action,
+          "Skipping, no matching action on changeset"
+        )
+
+        epic
+
+      changeset.action == :insert ->
+        case epic.assigns[:options][:creator] do
+          %{id: id} ->
+            maybe_debug(epic, act, id, "Casting explicit creator")
+            cast(epic, act, changeset, on, id)
+
+          id when is_binary(id) ->
+            maybe_debug(epic, act, id, "Casting explicit creator")
+            cast(epic, act, changeset, on, id)
+
+          nil ->
+            case current_user do
+              %{id: id} ->
+                Epics.smart(
+                  epic,
+                  act,
+                  current_user,
+                  "Casting current user as creator #{id}"
+                )
+
+                cast(epic, act, changeset, on, id)
+
+              id when is_binary(id) ->
+                maybe_debug(epic, act, id, "Casting current user as creator")
+                cast(epic, act, changeset, on, id)
+
+              other ->
+                Epics.smart(
+                  epic,
+                  act,
+                  current_user,
+                  "Skipping because of current_user"
+                )
+
+                epic
+            end
+
+          other ->
+            Epics.smart(epic, act, other, "Invalid custom creator")
+            epic
+        end
+
+      changeset.action == :delete ->
+        # TODO: deletion
+        epic
+    end
+  end
+
+  defp cast(epic, act, changeset, on, id) do
+    maybe_debug(epic, act, id, "Casting creator")
+
+    changeset
+    |> Changeset.cast(%{created: %{creator_id: id}}, [])
+    |> Changeset.cast_assoc(:created)
+    |> Epic.assign(epic, on, ...)
+  end
+end
