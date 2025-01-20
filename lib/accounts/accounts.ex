ed9ea698d657b7b59e54a18159abaa35187f1f8e
Mayel de Borniol <git@mayel.space>
2025-01-20 09:42:05 +0000

    gnore mailer timeouts

--- lib/accounts/accounts.ex
+++ lib/accounts/accounts.ex
@@ -872,6 +872,9 @@ defmodule Bonfire.Me.Accounts do
   ## misc
 
   defp mailer_response({:ok, _}, account), do: {:ok, account}
+  
+  defp mailer_response({:error, :timeout}, account),
+    do: {:ok, account} # we ignore mailer timeouts to avoid blocking the signup
 
   defp mailer_response({:error, error}, _) when is_atom(error),
     do: {:error, error}
